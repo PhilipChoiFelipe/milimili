fc9d88a7ab1806480656340dc5012ad85bf94d9b
Philip Choi <mschoi16@uw.edu>
2019-01-18 01:43:11 +0900

    Updates

---
+++ precache-manifest.5bc803fecd5d7b0d11f71a4808bb99d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85d7d679576855a05656",
+    "url": "/static/js/main.85d7d679.chunk.js"
+  },
+  {
+    "revision": "55527754ed690dfb9d67",
+    "url": "/static/js/1.55527754.chunk.js"
+  },
+  {
+    "revision": "85d7d679576855a05656",
+    "url": "/static/css/main.be9f9313.chunk.css"
+  },
+  {
+    "revision": "55527754ed690dfb9d67",
+    "url": "/static/css/1.6cc25569.chunk.css"
+  },
+  {
+    "revision": "56e9c211e0a2bd6e1369688561dcd59f",
+    "url": "/index.html"
+  }
+];
